8f3cdf69d89697948445901ca195a9898403a671
Yoav Landman <yoavl@jfrog.com>
2015-06-30 13:51:14 +0300

    Copy spec improvements - avoid relating to the project directory as the output dir

--- src/main/groovy/com/jfrog/bintray/gradle/RecordingCopyTask.groovy
+++ src/main/groovy/com/jfrog/bintray/gradle/RecordingCopyTask.groovy
@@ -1,10 +1,12 @@
 package com.jfrog.bintray.gradle
 
+import org.gradle.api.InvalidUserDataException
 import org.gradle.api.internal.file.copy.CopyAction
 import org.gradle.api.internal.file.copy.CopyActionProcessingStream
 import org.gradle.api.internal.file.copy.FileCopyDetailsInternal
 import org.gradle.api.internal.tasks.SimpleWorkResult
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.OutputDirectory
 
 class RecordingCopyTask extends Copy {
 
@@ -12,10 +14,17 @@ class RecordingCopyTask extends Copy {
 
     def fileUploads = []
 
+    @OutputDirectory
+    public File getDestinationDir() {
+        null
+    }
+
     @Override
     protected CopyAction createCopyAction() {
-        //Check for non-dir input and make the output path relative to the destination
-        def resolver = getFileLookup().getFileResolver(destinationDir)
+        def intoDir = getRootSpec().@destinationDir
+        if (intoDir instanceof String) {
+            throw new InvalidUserDataException("Bintray copy spec 'into' only accepts a relative string path.")
+        }
         return {
                 //CopyAction
             CopyActionProcessingStream stream ->
@@ -23,8 +32,8 @@ class RecordingCopyTask extends Copy {
                         //CopyActionProcessingStreamAction
                     FileCopyDetailsInternal details ->
                         if (!details.isDirectory()) {
-                            File target = resolver.resolve(details.getRelativePath().getPathString());
-                            def destRelPath = project.relativePath(target)
+                            //
+                            def destRelPath = intoDir != null ? (intoDir + '/' + details.getPath()) : details.getPath()
                             fileUploads << new Artifact(file: details.file, path: destRelPath)
                             didWork = true
                         }
